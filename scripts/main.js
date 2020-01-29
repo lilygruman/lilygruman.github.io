451db74303fc1e6d1c9119d5c189553663758b2e
Ben Gruman <bengruman@gmail.com>
2020-01-29 16:33:21 -0600

    Restructure
    Encapsulate oscillators and drawing functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 let page = document.querySelector('html');
 
+button.onclick = function() {}
 page.onclick = function() {
     alert('Get OUT of my WEBSITE');
     page.onclick = function(){};
+    button.onclick = function() {
+        title.textContent = 'Oh no you made it worse'
+    }
 }
 
 let button = document.querySelector('button');
 let title = document.querySelector('title');
-
-button.onclick = function() {
-    title.textContent = 'Oh no you made it worse'
-}
